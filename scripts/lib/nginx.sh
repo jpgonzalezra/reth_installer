21f6c80252252e35c34b602c33501ccfe0922141
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2025-03-29 19:11:14 -0300

    chore: nginx cleanup code

--- scripts/lib/nginx.sh
+++ scripts/lib/nginx.sh
@@ -6,7 +6,7 @@ create_nginx_proxy() {
   local target=$3
   local extra_config=$4
 
-  sudo bash -c "cat <<EOL > /etc/nginx/sites-available/${name}
+  sudo tee "/etc/nginx/sites-available/${name}" > /dev/null <<EOL
 server {
     listen ${port};
 
@@ -14,14 +14,14 @@ server {
         proxy_pass http://localhost:${target};
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
-        $( [[ -n "$extra_config" ]] && echo "$extra_config" )
+        $extra_config
 
         # Basic Authentication
-        auth_basic \"Restricted Content\";
+        auth_basic "Restricted Content";
         auth_basic_user_file /etc/nginx/.htpasswd;
     }
 }
-EOL"
+EOL
 
-  sudo ln -sf /etc/nginx/sites-available/${name} /etc/nginx/sites-enabled/${name}
+  sudo ln -sf "/etc/nginx/sites-available/${name}" "/etc/nginx/sites-enabled/${name}"
 }
