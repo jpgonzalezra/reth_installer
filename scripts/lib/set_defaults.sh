5f808d60482e873fb9b57ca7d0f14f1d489792b1
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2025-03-29 17:21:58 -0300

    fix: remove unnecessary default values

--- scripts/lib/set_defaults.sh
+++ scripts/lib/set_defaults.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # Default values for required vars
-: "${NODE_CLIENT:=reth}"
 : "${SYNC_FROM:=chain}"
-: "${BASE_DIR:=chain}"
-: "${RETH_VERSION:=0.1.0-alpha.1}"
-: "${LIGHTHOUSE_VERSION:=4.2.0}"
 : "${NETWORK:=mainnet}"
 : "${SNAPSHOT_TYPE:=archive}"
 : "${RETH_FLAGS:=debug,eth,net,trace,txpool,web3,rpc}"
