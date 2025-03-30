974ad3b1ddcdbe9a7ee12f7bd17d4f4fc6426872
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2025-03-30 19:44:04 -0300

    feat: extract snapshot if not already extracted

--- setup_node.sh
+++ setup_node.sh
@@ -60,14 +60,17 @@ if [[ $SYNC_FROM == "merkle" ]]; then
     echo -e "${GREEN}${BOLD}Snapshot Downloaded!${NORMAL}"
   fi
 
-  echo -e "${ORANGE}${BOLD}Extracting snapshot to:${NC} $TARGET_PROJECT_ROOT"
-  if ! command -v unzstd &> /dev/null; then
-    echo "unzstd could not be found. Please install zstd."
-    exit 1
+  if [[ -d "$TARGET_PROJECT_ROOT/db" ]]; then
+    echo -e "${GREEN}Snapshot already extracted at: $TARGET_PROJECT_ROOT${NC}"
+  else
+    echo -e "${ORANGE}${BOLD}Extracting snapshot to:${NC} $TARGET_PROJECT_ROOT"
+    if ! command -v unzstd &> /dev/null; then
+      echo "unzstd could not be found. Please install zstd."
+      exit 1
+    fi
+    tar -v --use-compress-program=unzstd -xf reth-latest.tar.zst -C "$TARGET_PROJECT_ROOT" | tee extract.log
+    echo -e "${GREEN}Snapshot restored successfully to: $TARGET_PROJECT_ROOT${NC}"
   fi
-  tar --use-compress-program=unzstd -xf "$SNAPSHOT_TO_EXTRACT" -C "$TARGET_PROJECT_ROOT"
-
-  echo -e "${GREEN}Snapshot restored successfully to: $TARGET_PROJECT_ROOT${NC}"
 
 elif [[ $SYNC_FROM == "chain" ]]; then
   echo -e "${ORANGE}${BOLD}Sync mode:${NC} ${RED}${SYNC_FROM}${NC}."
