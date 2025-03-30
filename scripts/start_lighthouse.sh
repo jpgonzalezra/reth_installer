453282a1b672ede5ae1517adf860412dddfbf553
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2025-03-30 20:11:41 -0300

    fix: start lighthouse script

--- scripts/start_lighthouse.sh
+++ scripts/start_lighthouse.sh
@@ -6,4 +6,26 @@ parsed_dir=$(get_project_root)
 source "${parsed_dir}/scripts/lib/load_variables.sh"
 source "${parsed_dir}/scripts/lib/get_os_arch.sh"
 
-sudo /$BASE_DIR/lighthouse/lighthouse beacon_node --datadir /$BASE_DIR/lighthouse/data --network $NETWORK --http --execution-endpoint http://localhost:8551 --execution-jwt /$BASE_DIR/$NODE_CLIENT/data/jwt.hex --checkpoint-sync-url https://mainnet-checkpoint-sync.stakely.io --disable-backfill-rate-limiting --genesis-backfill --disable-deposit-contract-sync --execution-timeout-multiplier 10 --historic-state-cache-size 4 --disable-lock-timeouts --listen-address :: --listen-address 0.0.0.0 --port 9909 --port6 9911 --discovery-port6 9999 --target-peers 200 --validator-monitor-auto --metrics
+sudo /$BASE_DIR/lighthouse/lighthouse beacon_node \
+  --datadir /$BASE_DIR/lighthouse/data \
+  --network $NETWORK \
+  --http \
+  --http-address 0.0.0.0 \
+  --http-port 5052 \
+  --metrics \
+  --metrics-address 0.0.0.0 \
+  --metrics-port 5005 \
+  --execution-endpoint http://localhost:8551 \
+  --execution-jwt /$BASE_DIR/$NODE_CLIENT/data/jwt.hex \
+  --checkpoint-sync-url https://mainnet-checkpoint-sync.stakely.io \
+  --disable-backfill-rate-limiting \
+  --genesis-backfill \
+  --disable-deposit-contract-sync \
+  --execution-timeout-multiplier 10 \
+  --historic-state-cache-size 4 \
+  --listen-address 0.0.0.0 \
+  --port 9909 \
+  --port6 9911 \
+  --discovery-port6 9999 \
+  --target-peers 200 \
+  --validator-monitor-auto
